289eaf8d78b7af13df9877c91ce542fa7f7dcc86
momijisama <oniisaamaa@gmail.com>
2020-08-07 02:18:47 +0100

    Add join date and join duration to memberinfo command

--- src/commands/memberinfo.js
+++ src/commands/memberinfo.js
@@ -1,6 +1,28 @@
 const Discord = require('discord.js');
 const moment = require("moment");
 
+function CalculateDurationBetweenDates(start_date, end_date)
+{
+  const intervals = ["years","months","days"];
+  let output_string = [];
+  let s_date = moment(start_date);
+  let e_date = moment(end_date);
+
+  //Find the difference between the two dates per interval, and then add it to e_date so on the next pass there is no difference in that interval
+  //This isolates the next interval (which won't include the previous interval in its diff)
+  for(let i = 0; i < intervals.length; i++)
+  {
+    let diff = s_date.diff(e_date, intervals[i]);
+    e_date.add(diff, intervals[i]);
+
+    //Build the array that will become the resulting string
+    output_string.push(diff + " " + intervals[i]);
+  }
+
+  output_string = output_string.join(", ");
+  return output_string;
+}
+
 module.exports =
 {
   name: "memberinfo",
@@ -20,23 +42,24 @@ module.exports =
           const guild_member = message.guild.member(input);
           const mention = message.mentions.members.first();
 
+          //Get the user based on the input
           if(guild_member) //If the input is a user ID
           {
-            member = guild_member.user;
+            member = guild_member;
           }
           else if(mention) //If the input is a user mention
           {
-            member = mention.user;
+            member = mention;
           }
           else //If the input is invalid
           {
-            message.channel.send("Member not found").catch(console.error);
+            message.channel.send("Member not found!! (｡•́︿•̀｡)").catch(console.error);
             return;
           }
         }
         else if(args.length === 0) //User is attempting to get the information of themselves
         {
-          member = message.author;
+          member = message.member;
         }
         else
         {
@@ -44,31 +67,17 @@ module.exports =
           return;
         }
 
-        //Calculate the age of the user
-        const current_date = moment(new Date());
-        let created_date = moment(member.createdAt);
-        const intervals = ["years","months","days"];
-        let member_age_string = [];
-
-        //Find the difference between the two dates per interval, and then add it to createdDate so on the next pass there is no difference in that interval
-        //This isolates the next interval (which won't include the previous interval in its diff)
-        for(let i = 0; i < intervals.length; i++)
-        {
-          let diff = current_date.diff(created_date, intervals[i]);
-          created_date.add(diff, intervals[i]);
-
-          //Build the array that will become the resulting string
-          member_age_string.push(diff + " " + intervals[i]);
-        }
-
-        member_age_string = member_age_string.join(", ");
+        member_age_string = CalculateDurationBetweenDates(new Date(), member.user.createdAt);
+        member_join_string = CalculateDurationBetweenDates(new Date(), member.joinedAt);
 
         //Construct and send the member info embed
         const member_info = new Discord.MessageEmbed()
           .setColor(0xe92134)
-          .setTitle(member.tag)
-          .setThumbnail(member.displayAvatarURL({dynamic: true}))
-          .addField("Creation Date:", member.createdAt.toUTCString(), false)
+          .setTitle(member.user.tag)
+          .setThumbnail(member.user.displayAvatarURL({dynamic: true}))
+          .addField("Joined At:", member.joinedAt.toUTCString(), false)
+          .addField("Member Length:", member_join_string, false)
+          .addField("Created At:", member.user.createdAt.toUTCString(), false)
           .addField("Account Age:", member_age_string, false)
           .addField("ID:", member.id, false)
 
