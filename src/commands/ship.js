6d73ca3da6e605c256e8485360ed3075cc3b2251
momijisama <oniisaamaa@gmail.com>
2021-06-15 02:58:46 +0100

    Fix rounding error

--- src/commands/ship.js
+++ src/commands/ship.js
@@ -18,18 +18,6 @@ module.exports =
           return;
         }
 
-        //Before we add the arguments to the seed, sort them so that any combination of left/right will be the same
-        var lovers = [args[0], args[1]];
-        lovers.sort();
-
-        // Create a seed with the current date, and the sorted names of the people being shipped.
-        const now = new Date();
-        const seed = [now.toDateString(), lovers[0], lovers[1]].join(":");
-
-        // Use seedrandom script to generate a seeded random, and transform into a "percentage"
-        const rng = seedrandom(seed);
-        const percentage = (rng() * 100).toPrecision(2);
-
         // Crap but here we check whether the argument is a ping, and if it is we replace the argument with the member's name instead
         // Unintended side effect of the function: this makes using IDs valid as GetMemberFromArgument also handles those
         const member = GetMemberFromArgument(message, args[0]);
@@ -44,6 +32,18 @@ module.exports =
         {
           args[1] = member1.displayName;
         }
+
+        //Before we add the arguments to the seed, sort them so that any combination of left/right will be the same
+        var lovers = [args[0], args[1]];
+        lovers.sort();
+
+        // Create a seed with the current date, and the sorted names of the people being shipped.
+        const now = new Date();
+        const seed = [now.toDateString(), lovers[0], lovers[1]].join(":");
+
+        // Use seedrandom script to generate a seeded random, and transform into a "percentage"
+        const rng = seedrandom(seed);
+        const percentage = (Math.round(rng() * 100));
 
         message.channel.send("**" + args[0] + "** & **" + args[1] + "**: " + percentage + "% Match").catch(console.error);
       }
