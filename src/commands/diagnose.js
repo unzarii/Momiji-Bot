e8d4e813f362f378d47bce2093f329a35546387b
Unzarii <oniisaamaa@gmail.com>
2022-11-08 16:27:24 +0000

    Improve diagnose command

--- src/commands/diagnose.js
+++ src/commands/diagnose.js
@@ -14,26 +14,49 @@ module.exports =
     // Unintended side effect of the function: this makes using IDs valid as GetMemberFromArgument also handles those
     const member = GetMemberFromArgument(message, args[0]);
 
-    //It probably is disgusting that I'm reassigning args like this but I don't need it anymore sooo
+    // It probably is disgusting that I'm reassigning args like this but I don't need it anymore sooo
     if (member)
     {
       args[0] = member.displayName;
     }
+    
+    let condition = "";
+    
+    // Get every arg after the name and turn it into a single variable.
+    args.forEach((arg, index) =>
+    {
+        if (index != 0)
+        {
+            // Add them all together
+            condition += arg;
+            
+            // Add a space if needed lol
+            if (index != args.length - 1)
+            {
+                condition += " ";
+            }
+        }
+    });
 
-    // Create a seed with the current date, the name of the person being diagnosed, and the "cringe" string so that it doesn't somehow overlap with other commands.
+    // Build a seed with the current date, the name of the person being diagnosed, and the "cringe" string so that it doesn't somehow overlap with other commands.
     const now = new Date();
-    const seed = [now.toDateString(), args[0], args[1], "cringe"].join(":");
+    const seed = [now.toDateString(), args[0], condition, "cringe"].join(":");
 
     // Use seedrandom script to generate a seeded random, and transform into a "percentage"
     const rng = seedrandom(seed);
     const percentage = (Math.round(rng() * 100));
     
-    // Build response
-    let response = "I diagnose **" + args[0] + "** to have a **" + args[1] + "** level of: " + percentage + "%!";
+    // Hardly perfect but it works mostly
+    // Just don't confuse it with the word "unit" (or else)
+    const vowels = ["a","e","i","o","u"];
+    let an_or_a = vowels.includes(Array.from(condition)[0]) ? "an" : "a";
+    
+    // Build response. I know, it's fucking disgusting
+    let response = "**" + args[0] + "** has "+ an_or_a + " **" + condition + "** level of: "+ percentage + "%! ";
     
     if (percentage > 80)
     {
-        response += "\n /!\\ Critical levels of **" + args[1] + "** detected! /!\\"
+        response += "\n /!\\ Critical levels of **" + condition + "** detected! /!\\"
     }
     
     message.channel.send(response).catch(console.error);
